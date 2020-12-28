eb4383b5970af214b8846dfd28769bebedbca90b
yuntao.cyt <ssh_github@163.com>
2020-12-28 11:44:57 +0800

    chore:init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button, Row, Col } from 'antd';
+
+export default class APP extends React.Component {
+  render() {
+    return <Button>Form</Button>;
+  }
+}
